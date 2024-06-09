0b0bcf0373b4bd2b0570d1b687809118ee7f236c
Ramazan Shira <ramazanshira1@gmail.com>
2024-06-09 17:30:18 +0200

    Menu images changed

--- src/Home/Hero/Hero.jsx
+++ src/Home/Hero/Hero.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { categories } from "../../constants";
 import "./hero.css";
+import dogs from "../../assets/images/dog.jpg";
+import cats from "../../assets/images/cat.jpg";
+import birds from "../../assets/images/birds.jpg";
 const Hero = () => {
   const navigate = useNavigate();
 
